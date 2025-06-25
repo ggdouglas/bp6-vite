64f18dec2d1746d182746fec177eeb0510c7a111
Gregory Douglas <gdouglas@palantir.com>
2025-06-25 08:19:46 -0400

    React router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import { OverlayExample } from "./examples/OverlayExample";
 import { Overlay2Example } from "./examples/Overlay2Example";
 import { DialogExample } from "./examples/DialogExample";
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  { path: "/dialog", element: <DialogExample /> },
+  { path: "/overlay", element: <OverlayExample /> },
+  { path: "/overlay2", element: <Overlay2Example /> },
+]);
 
 function App() {
   return (
     <div className="app">
       <div className="examples">
-        <OverlayExample />
-        <Overlay2Example />
-        <DialogExample />
+        <RouterProvider router={router} />
       </div>
     </div>
   );
